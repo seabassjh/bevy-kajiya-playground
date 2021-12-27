97acb6bcd6bece6d917dab840e967336ee598961
Seabass247 <sebjfk@gmail.com>
2021-12-27 17:24:06 -0500

    clean up

--- assets/shaders/rt/gbuffer.rmiss.hlsl
+++
@@ -1,5 +0,0 @@
-#include "../inc/pack_unpack.hlsl"
-#include "../inc/rt.hlsl"
-
-[shader("miss")]
-void main(inout GbufferRayPayload payload: SV_RayPayload) {}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::input::mouse::MouseMotion;
 use bevy::prelude::*;
 use bevy_kajiya::{EnvironmentSettings, KajiyaCamera, KajiyaCameraBundle, KajiyaMeshInstance, KajiyaMeshInstanceBundle, KajiyaMesh};
-use bevy_kajiya::{KajiyaRendererDefaultPlugins, KajiyaSceneDescriptor};
+use bevy_kajiya::{KajiyaRendererPlugins, KajiyaSceneDescriptor};
 use dolly::prelude::{CameraRig, Position, Smooth, YawPitch};
 
 fn main() {
@@ -16,10 +16,11 @@ fn main() {
             ..Default::default()
         })
         .insert_resource(KajiyaSceneDescriptor {
-            scene_name: "gas_stations".to_string(),
+            scene_name: "car".to_string(),
             ..Default::default()
         })
-        .add_plugins(KajiyaRendererDefaultPlugins)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(KajiyaRendererPlugins)
         .add_startup_system(setup_world)
         .add_system(sun_move)
         .add_system(rotator_system)
@@ -28,6 +29,10 @@ fn main() {
 }
 
 fn setup_world(mut commands: Commands, windows: Res<Windows>) {
+    
+    // Spawn an entity to control the kajiya renderer camera.  Only 1 camera is allowed at the moment.
+    // The cameara bundle also provides the EnvironmentSettings components to give the user access to
+    // the sun state.
     let window = windows.get_primary().unwrap();
     commands.spawn_bundle(KajiyaCameraBundle {
         camera: KajiyaCamera {
@@ -37,24 +42,41 @@ fn setup_world(mut commands: Commands, windows: Res<Windows>) {
         ..Default::default()
     });
 
+    // Not required, just a nice camera driver to give easy, smooth, camera controls.
     let camera_rig = CameraRig::builder()
-        .with(Position::new(dolly::glam::Vec3::ZERO))
+        .with(Position::new(dolly::glam::Vec3::new(0.0, 2.5, 10.0)))
         .with(YawPitch::new().rotation_quat(dolly::glam::Quat::IDENTITY))
         .with(Smooth::new_position_rotation(1.0, 1.0))
         .build();
 
     commands.insert_resource(camera_rig);
 
+    // Spawn a new "user" mesh instance with the "ring" mesh
     commands.spawn_bundle(KajiyaMeshInstanceBundle {
-        mesh_instance: KajiyaMeshInstance {
-            mesh: KajiyaMesh::User("336_lrm".to_string()),
+        mesh_instance: KajiyaMeshInstance { 
+            mesh: KajiyaMesh::User("ring".to_string()),
         },
-        transform: Transform::default(),
-    }).insert(Rotator);
+        transform: Transform::from_translation(Vec3::new(0.0, 1.0, 0.0)),
+    }).insert(Rotator {
+        ccw: true,
+    });
+
+    // Spawn a mesh instance entity that "attaches" to the instance created by the scene loader.
+    // Allows you to interact with the scene's meshes.  Scene meshes are referred to by their scene
+    // index and the mesh file name.  0 is the first mesh instance described in the .ron scene file, 
+    // 1 the second mesh instance... etc.
+    commands.spawn_bundle(KajiyaMeshInstanceBundle {
+        mesh_instance: KajiyaMeshInstance { 
+            mesh: KajiyaMesh::Scene(0, "336_lrm".to_string()),
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, -0.001, 0.0)),
+    }).insert(Rotator {
+        ccw: false,
+    });
 }
 
 fn sun_move(time: Res<Time>, mut query: Query<&mut EnvironmentSettings, With<KajiyaCamera>>) {
-    let scale = 0.00005;
+    let scale = 0.0005;
     for mut env in query.iter_mut() {
         let theta = 180.0 * (time.time_since_startup().as_secs_f32() * scale).cos();
         let phi = 180.0 * (time.time_since_startup().as_secs_f32() * scale).sin();
@@ -64,12 +86,20 @@ fn sun_move(time: Res<Time>, mut query: Query<&mut EnvironmentSettings, With<Kaj
 
 /// this component indicates what entities should rotate
 #[derive(Component, Clone)]
-struct Rotator;
+struct Rotator {
+    ccw: bool,
+}
 
 /// rotates the parent, which will result in the child also rotating
-fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
-    for mut transform in query.iter_mut() {
-        transform.rotation *= Quat::from_rotation_y(3.0 * time.delta_seconds());
+fn rotator_system(time: Res<Time>, mut query: Query<(&mut Transform, &Rotator)>) {
+    for (mut transform, rotator) in query.iter_mut() {
+        let ang_vel = if rotator.ccw {
+            1.0
+        } else {
+            -1.0
+        };
+
+        transform.rotation *= Quat::from_rotation_y(ang_vel * time.delta_seconds());
     }
 }
 
